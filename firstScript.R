0e401a4f23175c371b4ee5a1d6d0f5408ca047fd
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2019-12-04 15:52:31 +0000

    Update firstScript.R (#6)

--- firstScript.R
+++ firstScript.R
@@ -13,6 +13,7 @@ cor.test(dat$dat.x, dat$dat.y, method = "pearson")
 
 # Plot
 ggplot(data = dat,aes(x = dat.x, y = dat.y) ) +
-  geom_point(aes(x = dat.x, y = dat.y))
+  geom_point(aes(x = dat.x, y = dat.y)) +
+               theme_bw()
 
 write.table(dat, "full_data.csv", sep = ",", col.names = NA)
