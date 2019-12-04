67d95c36bea765e8744dc2514c180a925c45322c
charles-cunningham <cac567@york.ac.uk>
2019-12-04 15:06:14 +0000

    changes to dat, add ggplot

--- firstScript.R
+++ firstScript.R
@@ -1,3 +1,10 @@
-dat <- runif(100, 0, 100)
+library(ggplot2)
 
-plot(dat)
+#generate data
+dat.x <- runif(100, 0, 100)
+dat.y <- runif(100, 0, 100)
+dat <- data.frame(cbind(dat.x,dat.y))
+
+# Plot
+ggplot(data = dat,aes(x = dat.x, y = dat.y) ) +
+  geom_point(aes(x = dat.x, y = dat.y))
